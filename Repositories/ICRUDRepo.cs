4cbde45ad089dbf53f83a54b3ff75d2305c1e3af
AhmedMTwab <ahmedtwab1234@gmail.com>
2024-03-12 17:24:19 +0200

    repoCommit

---
+++ Repositories/ICRUDRepo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Egyptian_association_of_cieliac_patients.Repositories
+{
+    public interface ICRUDRepo<T> where T : class
+    {
+            T FindById(int id);
+
+            T SelectOne(Expression<Func<T, bool>> match);
+
+            IEnumerable<T> FindAll();
+
+            IEnumerable<T> FindAll(params string[] agers);
+
+            Task<T> FindByIdAsync(int id);
+
+            Task<IEnumerable<T>> FindAllAsync();
+
+            Task<IEnumerable<T>> FindAllAsync(params string[] agers);
+
+            void AddOne(T myItem);
+
+            void UpdateOne(T myItem);
+
+            void DeleteOne(T myItem);
+
+            void AddList(IEnumerable<T> myList);
+
+            void UpdateList(IEnumerable<T> myList);
+
+            void DeleteList(IEnumerable<T> myList);
+        }
+    }
--- obj/Debug/net8.0/Egyptian association of cieliac patients.AssemblyInfo.cs
+++ obj/Debug/net8.0/Egyptian association of cieliac patients.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Egyptian association of cieliac patients")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f9412abc6afbfd81d680be928c3d98a6f07113c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87a3274e88e44e0b6f249ee8ee12fecc08f24a67")]
 [assembly: System.Reflection.AssemblyProductAttribute("Egyptian association of cieliac patients")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Egyptian association of cieliac patients")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

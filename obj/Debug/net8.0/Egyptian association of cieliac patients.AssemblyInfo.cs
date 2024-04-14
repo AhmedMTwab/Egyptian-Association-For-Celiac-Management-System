620aa7b7692fa2d9a0eb1253124b6b789961e4e6
AhmedMTwab <ahmedtwab1234@gmail.com>
2024-04-14 20:52:14 +0200

    with relation

--- obj/Debug/net8.0/Egyptian association of cieliac patients.AssemblyInfo.cs
+++ obj/Debug/net8.0/Egyptian association of cieliac patients.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Egyptian association of cieliac patients")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9f5560973450cba68cba9c8a9823f73379522ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a45e2c1b7d609155163a1ac4d5c460b0ef7b4afa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Egyptian association of cieliac patients")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Egyptian association of cieliac patients")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

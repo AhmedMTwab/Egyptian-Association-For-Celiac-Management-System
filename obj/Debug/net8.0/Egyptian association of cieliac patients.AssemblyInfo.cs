b16e3b9a22d83176598809d58e36c0be98ec670c
AhmedMTwab <ahmedtwab1234@gmail.com>
2024-04-14 20:43:30 +0200

    before-merge

--- obj/Debug/net8.0/Egyptian association of cieliac patients.AssemblyInfo.cs
+++ obj/Debug/net8.0/Egyptian association of cieliac patients.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Egyptian association of cieliac patients")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+696804283e479ec15c454b0499f02e2fa6e367f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9f5560973450cba68cba9c8a9823f73379522ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Egyptian association of cieliac patients")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Egyptian association of cieliac patients")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

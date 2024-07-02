b74d7e37c3161b034ebdac0d6acad07bfd960a87
Ahmed Mohamed Badawy <am4752714@gmail.com>
2024-07-02 15:00:28 +0300

    order

--- obj/Debug/net8.0/Egyptian association of cieliac patients.AssemblyInfo.cs
+++ obj/Debug/net8.0/Egyptian association of cieliac patients.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Egyptian association of cieliac patients")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da281272650d000172d3004cf84119f01777a2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23ac5ce6e4c245a592aac5786d8247c211e707f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Egyptian association of cieliac patients")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Egyptian association of cieliac patients")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
